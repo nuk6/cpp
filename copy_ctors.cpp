753613408bf4579bbb51e60403de5b64bdc166f6
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-09-23 22:35:58 +0530

    Create copy_ctors.cpp

---
+++ copy_ctors.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class A{
+    int a;
+public:
+    A(){};
+    A(int a):a(a){}
+    int getA(){
+        return a;
+    }
+    int* getAddress(){
+        return &a;
+    }
+    //Here 'const' is not dead necessary, but its a good practice.
+    A(const A& a){
+        cout << "\nCopy Ctor Invoked!! \n";
+    }
+};
+
+
+int main(){
+    A a1(7);
+    cout << a1.getA();
+    A a2(a1); // copy ctor invoked
+    cout << endl << a2.getA();
+    cout << endl << a1.getAddress();
+    cout << endl << a2.getAddress();
+    return 0;
+}
+
+/*
+
+7
+Copy Ctor Invoked!! 
+
+7
+0x7fff1d22fe70
+0x7fff1d22fe80
+
+*/
+
