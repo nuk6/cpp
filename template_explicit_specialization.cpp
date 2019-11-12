d8b8438d65632642801af0fcf894dee9e510082a
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-11-13 00:03:31 +0530

    Explicit Specialization

---
+++ template_explicit_specialization.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+// TEMPLATE SPECIALIZATION
+/*
+Is done to differentiate for a particular type.
+Must ne done in .cpp file.
+*/
+template<typename T>
+T maxx(T x, T y){
+    cout << "Common fn" << endl;
+    return (x > y)?x:y;
+}
+
+//Explicit instantiation
+template string maxx(string x, string y); 
+
+//Explicit specialization
+template<> const char* maxx(const char* x, const char* y){
+    cout << "Specialized fn" << endl;
+    return strcmp(x,y) > 0? x:y;
+}
+
+int main(){
+    const char* a{"A"};
+    const char* b{"B"};
+    cout << maxx(a,b) << endl;
+    cout << maxx(3,4) << endl;
+    return 0;
+}
