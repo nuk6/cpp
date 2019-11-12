2d449695786094c3d1fc99e1a262cee20eff3db8
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-11-12 23:36:03 +0530

    Template argument deduction and instantiation

--- template_argument_deduction_and_instantiation.cpp
+++ template_argument_deduction_and_instantiation.cpp
@@ -1,1 +1,19 @@
+#include "iheaders.h"
 
+template<typename T>
+T maxx(T x, T y){
+    cout << typeid(T).name() << endl;
+    return (x > y)?x:y;
+}
+
+//maxx fn for type 'string' will be instantiated
+template string maxx(string x, string y); 
+
+int main(){
+    cout << maxx(2,3) << endl;
+    cout << maxx(2.3,3.4) << endl;
+    char (*pfn)(char,char) = maxx;
+    cout << pfn('a','b') << endl;
+    cout << maxx<double>(3,4) << endl;
+    return 0;
+}
