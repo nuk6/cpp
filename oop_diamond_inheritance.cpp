ff915f75e6473b780daef4aad41d9fd00f76d962
Nidhi <nidhi.kumari.met14@iitbhu.ac.in>
2019-11-22 00:14:54 +0530

    💎 inheritance

--- oop_diamond_inheritance.cpp
+++ oop_diamond_inheritance.cpp
@@ -19,27 +19,29 @@ public:
 int main(){
     C obj;
     cout << sizeof(obj)/sizeof(int) << endl; // 5
+    //obj.a = 9 error
     return 0;
 }
 /*
 
-CORRECT : JUST NOTE TO MAKK PROBLEMATIC CLASS(HERE A) AS VIRTUAL WHILE INHERITING
+CORRECT : JUST NOTE TO MARK PROBLEMATIC CLASS(HERE A) AS VIRTUAL WHILE INHERITING
 
 
 #include "iheaders.h"
+
 class A{
 public:
     int a;
 };
-class B1: virtual A{
+class B1: virtual public A{
 public:
     int b1;
 };
-class B2: virtual A{
+class B2: virtual public A{
 public:
     int b2;
 };
-class C: B1,B2{
+class C: public B1, public B2{
 public:
     int c;
 };
@@ -47,6 +49,8 @@ public:
 int main(){
     C obj;
     cout << sizeof(obj)/sizeof(int) << endl; // 5
+    cout << sizeof(C)/sizeof(int) << endl;
+    obj.a = 9; //works
     return 0;
 }
 
